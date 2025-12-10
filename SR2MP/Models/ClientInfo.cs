a99dcb514ba6cd088d5fea6781997b18612d85cd
py8 <74925512+pyeight@users.noreply.github.com>
2025-12-10 16:06:41 +0100

    Updated ClientInfo Model

--- SR2MP/Models/ClientInfo.cs
+++ SR2MP/Models/ClientInfo.cs
@@ -7,12 +7,15 @@ public class ClientInfo
     public IPEndPoint EndPoint { get; set; }
     private DateTime LastHeartbeat { get; set; }
     public string PlayerId { get; set; }
+    // Shark: Not sure if this is necessary
+    // public string isAuthenticated { get; set; }
 
     public ClientInfo(IPEndPoint endPoint, string playerId = "")
     {
         EndPoint = endPoint;
         LastHeartbeat = DateTime.UtcNow;
         PlayerId = playerId;
+        // Authenticated = isAuthenticated;
     }
 
     public void UpdateHeartbeat() => LastHeartbeat = DateTime.UtcNow;
